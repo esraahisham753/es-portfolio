652e2bdfdbd77523dd5ee53d875f8275a5a3b133
Esraa Abduallah <esraahisham753@gmail.com>
2025-11-15 18:02:24 +0200

    feat: implement About section layout with responsive grid and image

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,14 @@
 const About = () => {
   return (
-    <div>About</div>
+    <section id="about" className="c-space my-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:grid-rows-6 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
